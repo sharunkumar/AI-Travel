cb981f04f4cb9a493a8c0799d5a02b023742b703
Sharun <sharunksplus@gmail.com>
2023-07-30 15:00:24 -0400

    plugged in open ai functions from https://github.com/Zoranner/chatgpt-local

--- src/env.mjs
+++ src/env.mjs
@@ -23,6 +23,7 @@ export const env = createEnv({
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
+    OPENAI_API_MODEL: z.string(),
   },
 
   /**
@@ -45,6 +46,7 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    OPENAI_API_MODEL: process.env.OPENAI_API_MODEL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
